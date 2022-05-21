e4abafe8cb952ba4d60e669594d9dbda09ea1353
Box Supremacy <j@box.lol>
2022-05-21 13:00:10 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 
 const path = require('path'),
     fs = require('fs');
@@ -12,20 +12,23 @@ app.use(cors());
 const { htmlToText } = require('html-to-text');
 
 app.get('*', (req, res) => {
-    var file = fs.readFileSync(path.resolve(__dirname, 'public', 'noscript.html'));
-    file = htmlToText(file);
-    file = file.replace(
-        "FLASH IS DEAD\n\nBut it wasn't always that way.",
-        "-------- flashisdead.xyz --------\n\nFLASH IS DEAD\nBut it wasn't always that way.\n\n---------------"
-    );
-    file = file.split('\n\nONE THING YOU CAN DO IS PLACE A NICE BADGE ON YOUR PAGE.')[0];
-    file = file.replace(/\s\[.*\]/g, '');
-    file = file.replace(/HAS\nREACHED/g, 'HAS REACHED');
-    file = file.replace(/\n\n\n/g, '\n\n---------------\n\n');
-    file = file + '\n-------- flashisdead.xyz --------\n';
-    res.send(file);
+    if (req.rawHeaders[3].includes('curl')) {
+        var file = fs.readFileSync(path.resolve(__dirname, 'public', 'noscript.html'));
+        file = htmlToText(file);
+        file = file.replace(
+            "FLASH IS DEAD\n\nBut it wasn't always that way.",
+            "-------- flashisdead.xyz --------\n\nFLASH IS DEAD\nBut it wasn't always that way.\n\n---------------"
+        );
+        file = file
+            .split('\n\nONE THING YOU CAN DO IS PLACE A NICE BADGE ON YOUR PAGE.')[0]
+            .replace(/\s\[.*\]/g, '')
+            .replace(/HAS\nREACHED/g, 'HAS REACHED')
+            .replace(/\n\n\n/g, '\n\n---------------\n\n');
+        file = file + '\n-------- flashisdead.xyz --------\n';
+        return res.send(file);
+    }
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log(`Server is up at port ${port}`);
 });
